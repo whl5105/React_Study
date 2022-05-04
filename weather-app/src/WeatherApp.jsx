986c582aed19069f54ef77d8ec155e814278e26c
whl5105 <whl5105@naver.com>
2022-05-04 15:04:35 +0900

    기온 컴포넌트 생성

--- weather-app/src/WeatherApp.jsx
+++ weather-app/src/WeatherApp.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CurrentWeather from './CurrentWeather/CurrentWeather'
 import "./styles.css";
+import TempInfo from './TempInfo/TempInfo';
 
 function WeatherApp() {
   return (
       <div className='container'>
         <CurrentWeather/>
+        <TempInfo/>
       </div>
   )
 }
